a71a6da17f1bdb45b89105a5c819776910598e70
nadav1996700 <nadav1996700@gmail.com>
2022-06-20 14:39:14 +0300

    fix cors problem and activate axius requests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Auth from "./components/Auth/Auth";
-import Main from "./components/MainScreen/Main";
 
 function App() {
   return (
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,37 +1,119 @@
 import React from "react";
 import { useState } from "react";
 import "./Auth.css";
+const axios = require("axios");
 
 function Auth() {
   const [toggleClass, setToggleClass] = useState(false);
+  const [loginFormData, setLoginFormData] = useState({
+    login_email: "",
+    login_password: "",
+  });
+  const [registerFormData, setRegisterFormData] = useState({
+    name: "",
+    register_email: "",
+    register_password: "",
+    type: "",
+  });
 
   const container_class_handler = () => {
     setToggleClass(!toggleClass);
   };
 
+  const handleLogin = (event) => {
+    // Prevent page reload
+    event.preventDefault();
+
+    // check if user exist
+    const promise = axios({
+      method: "post",
+      url: "http://localhost:3001/user/login/",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      data: {
+        email: loginFormData.login_email,
+        password: loginFormData.login_password,
+      },
+    });
+    promise.then((response) => {
+      if (response.data === "success") {
+        console.log("login ok");
+      } else if (response.data === "failure") {
+        console.log("login failed");
+      }
+    });
+  };
+
+  const handleRegister = (event) => {};
+
   return (
     <div>
-      <h2>Sign in/up For The Game</h2>
+      <h2 style={{ color: "#ffff" }}>META CENTRALAND Game</h2>
       <div
         className={`container ${toggleClass ? "right-panel-active" : ""}`}
         id="container"
       >
         <div className="form-container sign-up-container">
-          <form action="#">
+          <form onSubmit={handleRegister}>
             <h1>Create Account</h1>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Sign Up</button>
+            <input
+              type="text"
+              placeholder="Name"
+              onChange={(e) =>
+                setRegisterFormData({
+                  ...registerFormData,
+                  name: e.target.value,
+                })
+              }
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              onChange={(e) =>
+                setRegisterFormData({
+                  ...registerFormData,
+                  register_email: e.target.value,
+                })
+              }
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(e) =>
+                setRegisterFormData({
+                  ...registerFormData,
+                  register_password: e.target.value,
+                })
+              }
+            />
+            <button type="submit">Sign Up</button>
           </form>
         </div>
         <div className="form-container sign-in-container">
-          <form action="#">
+          <form onSubmit={handleLogin}>
             <h1>Sign in</h1>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <a href="#">Forgot your password?</a>
-            <button>Sign In</button>
+            <input
+              type="email"
+              placeholder="Email"
+              onChange={(e) =>
+                setLoginFormData({
+                  ...loginFormData,
+                  login_email: e.target.value,
+                })
+              }
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              onChange={(e) =>
+                setLoginFormData({
+                  ...loginFormData,
+                  login_password: e.target.value,
+                })
+              }
+            />
+            <button type="submit">Sign In</button>
           </form>
         </div>
         <div className="overlay-container">
