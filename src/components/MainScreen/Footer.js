c8710e94cde59bb268e93b271fcdd76db35e20b1
nadav1996700 <nadav1996700@gmail.com>
2022-06-21 17:16:17 +0300

    change font

--- src/components/MainScreen/Footer.js
+++ src/components/MainScreen/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="center">
-      <p>&copy; created by Nadav Cohen</p>
+      <p className="footer">&copy; created by Nadav Cohen</p>
     </div>
   );
 }
--- src/components/MainScreen/Legend.js
+++ src/components/MainScreen/Legend.js
@@ -6,23 +6,23 @@ function Legend() {
     <ul>
       <li>
         <div className="colored-box" style={{ backgroundColor: "#A6A6AC" }} />
-        <h3>Road</h3>
+        <h3 className="legend_title">Road</h3>
       </li>
       <li>
         <div className="colored-box" style={{ backgroundColor: "#5BE539" }} />
-        <h3>Park</h3>
+        <h3 className="legend_title">Park</h3>
       </li>
       <li>
         <div className="colored-box" style={{ backgroundColor: "#7896D5" }} />
-        <h3>For Sale</h3>
+        <h3 className="legend_title">For Sale</h3>
       </li>
       <li>
         <div className="colored-box" style={{ backgroundColor: "#0439A9" }} />
-        <h3>Not For Sale</h3>
+        <h3 className="legend_title">Not For Sale</h3>
       </li>
       <li>
         <div className="colored-box" style={{ backgroundColor: "#EC1512" }} />
-        <h3>My Lands</h3>
+        <h3 className="legend_title">My Lands</h3>
       </li>
     </ul>
   );
